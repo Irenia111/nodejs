9ddaac4f10fbebca82559c8030b08d664404f4e0
Irenia <15000616693@163.com>
2020-03-01 22:54:05 +0800

    newversion of http-server

--- server.js
+++ server.js
@@ -20,32 +20,39 @@ var server = http.createServer(function(request, response){
   /******** 从这里开始看，上面不要看 ************/
 
   console.log('有个人发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
+    response.statusCode = 200
 
-  if(path === '/'){
-    response.statusCode = 200
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`<!DOCTYPE html>
-    <html lang="zh">
-    <head>
-    <meta charset="utf-8" >
-    <link rel="stylesheet" href="/x">
-    </head>
-    <body>
-    <h1>你好</h1>
-    </body>
-    </html>`)
+    //获得请求文件的路径，如果请求的是根目录，则返回index.html
+    const filePath = path === "/" ? "/index.html" : path;
+
+    //根据请求的根目录文件后缀设置header的text类型
+    //1.fileType中为filePath中索引到的最后一个"."后的字符串
+    const fileType = filePath.lastIndexOf(".");
+    //2.建立哈希表，文件类型对应text
+    const fileHash ={
+      ".html":"text/html",
+      ".css" :"text/css",
+      ".js"  :"text/javascript",
+      ".json":"text/json",
+      ".xml" :"text/xml",
+      ".png" :"image/png",
+      ".jpg" :"image/jpeg"
+    }
+    //3.如果读取失败，则默认为text/html
+    response.setHeader('Content-Type', `${fileHash[fileType] || "text/html"};charset=utf-8`)
+
+    //读取对应的文件，若文件不存在，则输出404
+    let content
+    try{
+      content = fs.readFileSync(`./src${filePath}`);
+    }catch(error){
+      content = "文件不存在";
+      response.write(`你访问的页面不存在`)
+      response.statusCode = 404
+    } 
+
+    response.write(content)
     response.end()
-  } else if(path === '/x'){
-    response.statusCode = 200
-    response.setHeader('Content-Type', 'text/css;charset=utf-8')
-    response.write(`body{color: red;}`)
-    response.end()
-  } else {
-    response.statusCode = 404
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`你访问的页面不存在`)
-    response.end()
-  }
 
   /******** 代码结束，下面不要看 ************/
 })
